08c4a3c2e1a7ff13ddd6d80c4dc58ebb5f6376c8
Mithrandiirr <swimox17@protonmail.com>
2023-04-25 16:47:14 +0100

    commit

--- portfolio/desk/projectStructure.ts
+++
@@ -1,9 +0,0 @@
-import { ListItemBuilder } from "sanity/desk";
-import defineStructure from "../utils/defineStructure";
-
-export default defineStructure<ListItemBuilder>((S) =>
-S.listItem()
-.title('Projects')
-.schemaType('projects')
-.child(S.documentTypeList('projects'))
-)
--- portfolio/utils/defineStructure.ts
+++
@@ -1,9 +0,0 @@
-import { ConfigContext } from "sanity";
-import { StructureBuilder } from "sanity/desk";
-
-
-export default function defineStructure<StructureType>(
-    factory: (S: StructureBuilder, context: ConfigContext) => StructureType
-) {
-    return factory
-}
