52aeb6ee0fbf1eebd8b0f51d95b42ca5dc7bc118
Mohammed El Yaakoubi <88836550+Mithrandiirr@users.noreply.github.com>
2023-07-14 12:36:45 +0100

    Update sanity.ts

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -1,7 +1,7 @@
 import {createClient} from '@sanity/client'
 
 export default createClient({
-  projectId: 'ryhdnq2r',
+  projectId: 'xxxxxxxx', //change this to your sanity's projectId
   dataset: 'production',
   useCdn: true, // set to `true` to fetch from edge cache
   apiVersion: '2022-01-12', // use current date (YYYY-MM-DD) to target the latest API version
